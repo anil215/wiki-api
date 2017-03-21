9211ae8d6a0f56ed3c57f80bc66ff517a9455acf
anil215 <anilbit215@gmail.com>
2017-03-22 04:10:18 +0530

    Hash password before saving

--- testing/hashing.js
+++ testing/hashing.js
@@ -1,5 +1,6 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
 // THE THING BELOW IS ALREADY IMPLEMENTED AS JWT
 //
@@ -39,3 +40,16 @@ console.log(token);
 
 var decoded = jwt.verify(token,'secret');
 console.log(decoded);
+
+
+var password = '123abc!';
+bcrypt.genSalt(10,(err,salt) => {
+  bcrypt.hash(password,salt,(err,hash) => {
+    console.log(hash);
+  });
+});
+
+var hashedPassword = '$2a$10$QfnyzsJOAX9i1Lv.WCjS6eAqvq39gaD9mLwnsZTMCenfQeisFOG/e';
+bcrypt.compare(password,hashedPassword,(err,res) => {
+  console.log(res);
+});
